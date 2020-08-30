a08988ffbb040c5461668eccd3a9da97f6d7e479
Nikhil_7890 <nikhil.saxena_cs19@gla.ac.in>
2020-08-30 17:45:05 +0530

    call setBooksInLibrary() method which is available in Library.java

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -6,13 +6,24 @@
  * */
 package execution;
 
+import definitions.Library;
+import definitions.Student;
+
+import java.util.Scanner;
+
 public class FrontDesk {
-    private static final int Issue_a_new_Book_for_me = 1;
-    private static final int Return_a_Previously_Issued_Book_for_me = 2;
+    private static final int Issue_a_new_Book = 1;
+    private static final int Return_a_Previously_Issued_Book_First = 2;
     private static final int Show_me_all_my_Issued_Books = 3;
     private static final int Exit = 4;
 }
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int studentChoice;
+        Student student = new Student();
+        Library library = new Library();
+        library.setBooksInLibrary();
+        String bookName;
 
     }
