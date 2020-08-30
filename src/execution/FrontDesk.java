9430a10bc7a9972df6493c998353c41da1db60e4
Nikhil_7890 <nikhil.saxena_cs19@gla.ac.in>
2020-08-30 17:37:35 +0530

    Add fields to FrontDesk.java

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -1,12 +1,18 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
- *  Time: 3:50 PM
+ *  User: Nikhil Saxena (191500501)
+ *  Date: 29/08/20
+ *  Time: 8:25 PM
  *  File Name : FrontDesk.java
  * */
 package execution;
 
 public class FrontDesk {
+    private static final int Issue_a_new_Book_for_me = 1;
+    private static final int Return_a_Previously_Issued_Book_for_me = 2;
+    private static final int Show_me_all_my_Issued_Books = 3;
+    private static final int Exit = 4;
+}
+
     public static void main(String[] args) {
 
     }
